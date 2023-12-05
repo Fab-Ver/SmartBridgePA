94e77bce6ec2eaab9bff73814981cbf58b88bf70
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-12-05 08:54:04 +0100

    Implement tick method, add state enum

--- SmartBridgePA/lib/tasks/BlinkTask.cpp
+++ SmartBridgePA/lib/tasks/BlinkTask.cpp
@@ -1,7 +1,10 @@
 #include "BlinkTask.h"
 
+unsigned long lastBlink = 0;
+
 BlinkTask::BlinkTask(int ledPin){
     this->led = new Led(ledPin);
+    this->currBlinkState = BLINK_OFF;
 }
 
 void BlinkTask::init(){
@@ -14,5 +17,24 @@ void BlinkTask::tickWrapper(void* _this){
 }
 
 void BlinkTask::tick(){
-
+    for(;;){
+		unsigned long now = millis();
+        xSemaphoreTake(xMutex, portMAX_DELAY);
+		currBlinkState = currWaterLevelState != PRE_ALARM ? BLINK_OFF : currBlinkState == BLINK_OFF ? ON : currBlinkState;
+		xSemaphoreGive(xMutex);
+		if(now - lastBlink >= BLINK_PERIOD && currBlinkState != BLINK_OFF){
+			lastBlink = now;
+			if(currBlinkState == ON){
+                led->switchOff();
+                currBlinkState = OFF;
+            } else if (currBlinkState == OFF){
+                led->switchOn();
+                currBlinkState = ON;
+            }
+		} else if (now - lastBlink >= BLINK_PERIOD && currBlinkState == BLINK_OFF){
+            lastBlink = now;
+            led->switchOff();
+        }
+		vTaskDelay(500);
+  	}
 }
--- SmartBridgePA/lib/tasks/BlinkTask.h
+++ SmartBridgePA/lib/tasks/BlinkTask.h
@@ -4,11 +4,14 @@
 #include "Task.h"
 #include "Led.h"
 #include <Arduino.h>
+#include "config.h"
+#include "commons.h"
 
 class BlinkTask : public Task {
     private:
         Light* led;
         TaskHandle_t BlinkHandle;
+        BlinkState currBlinkState;
         void tick();
         static void tickWrapper(void* _this);
     public:
