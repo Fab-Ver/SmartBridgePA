60599e16a31dd834b1292d938578e06066caff7b
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-12-12 23:05:28 +0100

    Add components ports numbers

--- SmartBridgePA/lib/utils/config.h
+++ SmartBridgePA/lib/utils/config.h
@@ -4,21 +4,22 @@
 #define BLINK_PERIOD 2000
 #define SMART_LIGHT_PERIOD 200
 #define WATER_LEVEL_PERIOD 200
+#define COMMUNICATION_PERIOD 5000
 
 #define WL1 70
 #define WL2 40
 #define WL_MAX 2
 
-#define TRIG_PIN 0 
-#define ECHO_PIN 0 
-#define VALVE_PIN 0
-#define GREEN_PIN 0 
-#define RED_PIN 0 
-#define KNOB_PIN 0
+#define TRIG_PIN 37 
+#define ECHO_PIN 36
+#define VALVE_PIN 10
+#define GREEN_PIN 5
+#define RED_PIN 18 
+#define KNOB_PIN 15
 
-#define LED_PIN 0 
-#define LS_PIN 0 
-#define MS_PIN 0
+#define LED_PIN 6 
+#define LS_PIN 3
+#define MS_PIN 46
 
 typedef enum BlinkState {ON, OFF, BLINK_OFF} BlinkState;
 typedef enum SmartLightState {LIGHT_ON, LIGHT_OFF, WAITING, SYS_OFF} SmartLightState;
