f9cf8b3ba9ff629bd3c5dfbc49a103bcaeb8f561
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-12-12 23:04:54 +0100

    Add util file with function to print enum names

---
+++ SmartBridgePA/lib/utils/util.h
@@ -0,0 +1,27 @@
+#ifndef __UTIL__
+#define __UTIL__
+
+#include "config.h"
+#include "Arduino.h"
+
+String convertBlinkState(BlinkState value){
+    String names[3] = {"ON", "OFF", "BLINK_OFF"};
+    return names[value];
+}
+
+String convertSmartLightState(SmartLightState value){
+    String names[4] = {"LIGHT_ON", "LIGHT_OFF", "WAITING", "SYS_OFF"};
+    return names[value];
+}
+
+String convertWaterLevelState(WaterLevelState value){
+    String names[4] = {"NORMAL", "PRE_ALARM", "ALARM", "MANUAL"};
+    return names[value];
+} 
+
+String convertManualState(ManualState value){
+    String names[2] = {"MANUAL_ON", "MANUAL_OFF"};
+    return names[value];
+} 
+
+#endif
